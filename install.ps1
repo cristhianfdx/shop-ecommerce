e2ad2dbd050828786eb2ec0c51d6bad11ad8d23d
Cristhian Forero <cristhian.forero.dominguez@gmail.com>
2020-03-15 20:12:08 -0500

    Ajuste para windows

--- install.ps1
+++ install.ps1
@@ -2,7 +2,8 @@ Write-Host "Installing WordPress..."
 Start-Sleep -s 5
 docker exec wordpress /bin/sh -c "cat /var/www/html/.media-size-configuration > /var/www/html/.htaccess"
 
-docker exec wordpress /bin/sh -c 'sleep 10; wp core install --path="/var/www/html" --url="http://localhost:8080" --title="Tienda de Hardware El arduino" --admin_user=elarduino --admin_password=Elarduino46*_* --admin_email=elarduino46@gmail.com'
+Start-Sleep -s 10
+docker exec wordpress /bin/sh -c 'wp core install --path="/var/www/html" --url="http://localhost:8080" --title="Tienda de Hardware El arduino" --admin_user=elarduino --admin_password=Elarduino46*_* --admin_email=elarduino46@gmail.com'
 
 Write-Host "Installing WordPress Plugins..."
 Start-Sleep -s 5
